a4da0ff6f190ea6347aafbaf2b4c92df2c2acbe9
federicodececco <federicodececco1@gmail.com>
2024-12-17 12:16:37 +0100

    first

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,7 @@
-const names = ['Edoardo', 'Simone', 'Francesco'];
+const names = ["Edoardo", "Simone", "Francesco"];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+names.forEach((element) => {
+  console.log(element);
+});
